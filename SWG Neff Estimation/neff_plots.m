33c05ddea3534bf89b69bd337ff1fb9817a6f9ba
Bruno Taglietti <brunotaglietti@gmail.com>
2021-10-31 19:39:14 -0400

    Organizes hard sweep into another script. EME did not work.

--- SWG Neff Estimation/neff_plots.m
+++ SWG Neff Estimation/neff_plots.m
@@ -3,22 +3,19 @@ load('./wdm_coupler/SWG Neff Estimation/neff_sweep.mat');
 widths = widths*1e9;
 
 figure(2);
-% subplot 121
+subplot 121
 plot(widths, neffs), hold on, box off, grid on
 xlim([widths(1) 1500]);
 ylim([1.4 max(neffs(:))]);
 xlabel('width [nm]');
 ylabel('$n_{eff}$');
 
-% clearvars;
-% load('TM_neff_sweep.mat');
-% widths = widths*1e9;
-% subplot 122
-% plot(widths, neffs), hold on, box off, grid on
-% xlim([widths(1) 1500]);
-% ylim([1.4 max(neffs(:))]);
-% xlabel('width [nm]');
-% ylabel('$n_{eff}$');
-
-nac = interp1(widths, neffs(1,:), 500);
-w1 = interp1(neffs(2,:), widths, nac)
+clearvars;
+load('./wdm_coupler/SWG Neff Estimation/TM_neff_sweep_b1.mat');
+widths = widths*1e9;
+subplot 122
+plot(widths, neffs), hold on, box off, grid on
+xlim([widths(1) 1500]);
+ylim([1.4 max(neffs(:))]);
+xlabel('width [nm]');
+ylabel('$n_{eff}$');
